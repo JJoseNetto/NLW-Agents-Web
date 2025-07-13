c9d2289917c8f9342f9a47e5a12ec51fb541aac8
jjose <jjosenetto2020n@gmail.com>
2025-07-13 14:36:27 -0300

    add finaly configuration for perfomace complete in audio --save progress

--- src/components/question-form.tsx
+++ src/components/question-form.tsx
@@ -49,6 +49,8 @@ export function QuestionForm({ roomId }: QuestionFormProps) {
     await createQuestion(data)
   }
 
+  const { isSubmitting } = form.formState
+
   return (
     <Card>
       <CardHeader>
@@ -70,8 +72,9 @@ export function QuestionForm({ roomId }: QuestionFormProps) {
                 <FormItem>
                   <FormLabel>Sua Pergunta</FormLabel>
                   <FormControl>
-                    <Textarea
+                    <Textarea 
                       className="min-h-[100px]"
+                      disabled={isSubmitting}
                       placeholder="O que você gostaria de saber?"
                       {...field}
                     />
@@ -81,7 +84,7 @@ export function QuestionForm({ roomId }: QuestionFormProps) {
               )}
             />
 
-            <Button type="submit">Enviar pergunta</Button>
+            <Button disabled={isSubmitting} type="submit">Enviar pergunta</Button>
           </form>
         </Form>
       </CardContent>
